9da7ab935c41fc2de8189e55f227b024faaa83a5
Mario J <mjere003@gmail.com>
2022-07-08 21:29:10 -0400

    added comments

--- peteTheBaker.py
+++ peteTheBaker.py
@@ -9,13 +9,13 @@ For simplicity there are no units for the amounts
 
 
 def cakes(recipe, available):
-    total = []
-    for k in recipe:
-        if k in available:
-            total.append((available[k]//recipe[k]))
+    total = []  # Store how many cakes could be made from the specific ingredients
+    for k in recipe:    # Checks if recipe key in available
+        if k in available:    # If the recipe key is available, divide recipe amount from total available
+            total.append((available[k]//recipe[k]))  # Add the amount to total list
         else:
-            total.append(0)
-    return min(total)
+            total.append(0)  # If the recipe key is not available, append 0
+    return min(total)   # Return the smallest value b/c this will dictate how many complete cakes can be made
 
 
 print(cakes({"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}, {"sugar": 500, "flour": 2000, "milk": 2000}))
